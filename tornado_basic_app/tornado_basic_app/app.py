c8c77d345433c0afd07032ea35d412745cf1c7e8
Pablo ( Paulo )  Lopez <8420448+pau-lo@users.noreply.github.com>
2018-04-06 15:53:21 -0700

    Add files via upload

---
+++ tornado_basic_app/tornado_basic_app/app.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+import tornado.web
+
+# route to index channel
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('templates/home.html')
+# self.write("Hello, world")
+
+# r"/" == root website address
+# where to find the static files , / root address
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ], debug=True)
+
+
+# start the server as a port
+if __name__ == "__main__":
+    app = make_app()
+    PortNumber = str(8888)
+    print(r'App server now running.......')
+    print(r'Server Running at http://localhost:' + PortNumber + r'/')
+    print(r'To close press ctrl + c')
+    app.listen(PortNumber)
+    tornado.ioloop.IOLoop.current().start()
